84773063477900c6e39ac8f5781aad029b43902e
NamesSR <40932@ma-web.nl>
2026-08-14 20:53:11 +0200

    index starts at 0

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #![allow(non_snake_case)]
+#![allow(nonstandard_style)]
 
-use std::{collections::{HashMap, hash_map}, io};
-use std::env;
+
+use std::{collections::{HashMap}, io};
+
  #[derive(Debug)]
 enum BTWType {
     
@@ -24,21 +26,24 @@ struct Lables{
 
 fn main() {
    
-   
-    readLine();
-//     let mut LableList: HashMap<u32,Lables> = HashMap::new();
-//     LableList.insert(0001, Lables { Productnum: (0001), PriceIncBTW: (9.99), PriceExuBTW: (9.99), Name: (String::from("Cable")), Btw: (BTWType::none) });
-//    match LableList.get(&0001) {
-//     Some(lable) => println!("{:?}", lable),
-//     None => println!("there are no lable's under that Productnum")
+   for i in 0..2 {
        
-//    }
+       ReadCommand();
+   }
+       
+   
+    let mut LableList: HashMap<u32,Lables> = HashMap::new();
+    LableList.insert(0001, Lables { Productnum: (0001), PriceIncBTW: (9.99), PriceExuBTW: (9.99), Name: (String::from("Cable")), Btw: (BTWType::none) });
+   match LableList.get(&0001) {
+    Some(lable) => println!("{:?}", lable),
+    None => println!("there are no lable's under that Productnum")
+       
+   }
 }
 
-fn readLine()
+fn ReadCommand()
 {
  let mut args: Vec<String> = Vec::new();
-  args.push(" ".to_string());
  let mut input = String::new();
     io::stdin()
         .read_line(&mut input)  // 2. Pass buffer as a mutable reference
@@ -57,14 +62,15 @@ fn readLine()
     
     }
    
-    if args.len() > 2 {
-        println!("{} {}", args[1], args[2]);
+    if args.len() > 1 {
+        println!("{} {}", args[0], args[1]);
     } else {
-         println!("{}", args[1]);
+        
             println!("No argument provided.");
         
     }
     args.clear();
+    //let mut args: Vec<String> = Vec::new();
     let mut i = 1;
     
 }
